bfced2ea3cf3477a1248796d2acf442fc899fe18
Siert <s.j.g.sebus@student.tudelft.nl>
2022-12-15 15:25:50 +0100

    Implement dynamic coloring based on time range.

--- world-map/map.js
+++ world-map/map.js
@@ -8,6 +8,9 @@
   let mapTranslateX = 0;
   let mapTranslateY = 0;
   const volcanoIconSize = 8;
+  let eruptionTimeRange = [0, 200];
+  const volcanoEruptedColor = "red";
+  const volcanoNotEruptedColor = "black";
 
   let mapSvg = d3.select('#worldMap')
     .append("svg")
@@ -22,21 +25,8 @@
 
   let projection = d3.geoMercator()
     .center([2, 47]) // GPS of location to zoom on
-    .scale(mapZoom)  // This is like the zoom
+    .scale(mapZoom)
     .translate([ mapWidth / 2, mapHeight / 2 ]);
-
-  // populate svg with volcanoes
-  d3.csv(dataPath + "GVP_Volcano_List.csv").then(function(volcanoData) {
-    volcanoes.selectAll("circle")
-    .data(volcanoData)
-    .enter()
-    .append("circle")
-      .attr("cx", d => projection([d.Longitude, d.Latitude])[0])
-      .attr("cy", d => projection([d.Longitude, d.Latitude])[1])
-      .attr("r", volcanoIconSize)
-    
-    setupTooltip();
-  });
 
   // populate svg with countries using the loaded geoData
   d3.json(dataPath + "custom.geo.json").then(function(geoData) {
@@ -51,8 +41,21 @@
       .style("stroke", "#606060");
   });
 
+  // populate svg with volcanoes
+  d3.csv(dataPath + "GVP_Volcano_List.csv").then(function(volcanoData) {
+      volcanoes.selectAll("circle")
+        .data(volcanoData)
+        .enter()
+        .append("circle")
+          .attr("cx", d => projection([d.Longitude, d.Latitude])[0])
+          .attr("cy", d => projection([d.Longitude, d.Latitude])[1])
+          .attr("r", volcanoIconSize);
+        
+      setupTooltip();
+  });
+
   // map update function for when projection changes
-  function updateMap() {
+  function updateMapView() {
     projection
       .scale(mapZoom)
       .translate([mapWidth / 2 + mapTranslateX * mapZoom, mapHeight / 2 + mapTranslateY * mapZoom]);
@@ -67,10 +70,58 @@
     .attr("cy", d => projection([d.Longitude, d.Latitude])[1])
   }
 
+  // map update function for when eruption time range changes (defined below)
+  let updateTimeRange = () => undefined;
+
+  d3.csv(dataPath + "GVP_Volcano_List.csv").then(function(volcanoData) {
+    d3.csv(dataPath + "GVP_Eruption_Results.csv").then(function(eruptionData) {
+      
+      // set up eruption and volcano-eruption indices for quick lookup
+      let eruptionIndex = {};
+      let volcanoEruptionIndex = {};
+
+      // fill the volcano-eruption index with empty lists
+      volcanoData.forEach(volcano => {
+        let volcanoId = volcano.Volcano_Number;
+        volcanoEruptionIndex[volcanoId] = [];
+      });
+
+      eruptionData.forEach(eruption => {
+        let volcanoId = eruption["Volcano Number"];
+        let eruptionId = eruption["Eruption Number"];
+        
+        // include in eruption index
+        eruptionIndex[eruptionId] = eruption;
+
+        // include in volcano-eruption index
+        if (volcanoEruptionIndex[volcanoId] != undefined)
+          volcanoEruptionIndex[volcanoId].push(eruptionId);
+      });
+
+      updateTimeRange = () => {
+        volcanoes.selectAll("circle")
+          .style("fill", d => {
+            let eruptions = volcanoEruptionIndex[d.Volcano_Number];
+            
+            let inRange = eruptions.some(eruptionId => {
+              let eruption = eruptionIndex[eruptionId];
+              
+              return parseFloat(eruption["Start Year"]) >= eruptionTimeRange[0] 
+                && parseFloat(eruption["Start Year"]) <= eruptionTimeRange[1]
+            });
+
+            return inRange ? volcanoEruptedColor : volcanoNotEruptedColor;
+          });
+      };
+
+      updateTimeRange();
+    })
+  });
+
   // set up zoom input
   mapSvg.on("wheel", (e) => {
     mapZoom = mapZoom ** (1 - e.deltaY * mapZoomfactor);
-    updateMap();
+    updateMapView();
     return false;
   });
 
@@ -79,23 +130,25 @@
     if (e.buttons == 1) { // check if left-button is held
       mapTranslateX += e.movementX / mapZoom;
       mapTranslateY += e.movementY / mapZoom;
-      updateMap();
+      updateMapView();
     }
   });
-
-  // create tooltip div
-  let tooltip = d3.select("#worldMap")
-    .append("div")
-    .attr("id", "tooltip");
   
   // add tooltip functionality
   function setupTooltip() {
+    
+    // create tooltip div
+    let tooltip = d3.select("#worldMap")
+      .append("div")
+      .attr("id", "tooltip");
+    
+    // add mouse interaction to volcanoes
     volcanoes.selectAll("circle")
       .on("mouseover", () => tooltip.style("opacity", 1))
       .on("mouseleave", () => tooltip.style("opacity", 0))
       .on("mousemove", (e, d) => {
         tooltip.html(d.Volcano_Name + "<br>" + "Longitude: " + d.Longitude + "<br>" + 
-                    "Latitude: " + d.Latitude)
+                    "Latitude: " + d.Latitude + "<br>Last eruption year: " + d.Last_Eruption_Year)
         .style("left", (e.pageX+10) + "px")
         .style("top", e.pageY + "px");
       });
