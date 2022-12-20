cab95cf0e562d527e114f80b7a8d07b0b2d1c691
Siert <s.j.g.sebus@student.tudelft.nl>
2022-12-20 14:46:25 +0100

    Finish region selection and add interface for it

---
+++ world-map/demo.js
@@ -0,0 +1,36 @@
+// @ts-check
+
+{
+  // The global variables from map.js (these might be undefined at start):
+  console.log("DEMO CODE RUNNING!");
+  console.log("volcanoes:", volcanoes);
+  console.log("isVolcanoInsideRegion:", isVolcanoInsideRegion);
+  console.log("onRegionAdjustEvents:", onRegionAdjustEvents);
+  console.log("onMapReadyEvents:", onMapReadyEvents);
+
+
+  // define a function to be called on every region adjustement that updates the volcano list
+  function update() {
+
+    // create a list of selected volcano names
+    let selectedVolcanoNames = [];
+
+    // refill the volcano names array with the updated selection
+    volcanoes
+      .selectAll("circle")
+      .each((d, i, circles) => {
+        let c = d3.select(circles[i]);
+        if (isVolcanoInsideRegion(c))
+          selectedVolcanoNames.push(d.Volcano_Name);
+      });
+
+    console.log(selectedVolcanoNames);
+  }  
+
+  // add our function to the list of "region adjust" events (in order to have it be called on each 
+  // user adjustment to the region)
+  onRegionAdjustEvents.push(update);
+
+  // also add it to the "map ready" events so that it is called once on map load
+  onMapReadyEvents.push(update);
+}
--- world-map/map.js
+++ world-map/map.js
@@ -1,4 +1,15 @@
 // @ts-check
+
+// global variables
+// @ts-ignore
+let volcanoes = undefined;
+// @ts-ignore
+let isVolcanoInsideRegion = (c) => undefined;
+// @ts-ignore
+let onRegionAdjustEvents = [];
+// @ts-ignore
+let onMapReadyEvents = [];
+
 {
   const dataPath = "../data/";
   const mapWidth = 800;
@@ -8,10 +19,9 @@
   let mapTranslateX = 0;
   let mapTranslateY = 0;
   const volcanoIconSize = 8;
-  let eruptionTimeRange = [0, 200];
   const volcanoEruptedColor = "red";
   const volcanoNotEruptedColor = "black";
-  let regionSelectMode = false;
+  let regionSelectMode = true;
   let regionMin = [0, 0];
   let regionMax = [0, 0];
 
@@ -23,9 +33,9 @@
 
   let countries = mapSvg.append("g")
     .attr("id", "countries");
-  let brushSelection = mapSvg.append("g")
-    .attr("id", "brushSelection");
-  let volcanoes = mapSvg.append("g")
+  let regionSelection = mapSvg.append("g")
+    .attr("id", "regionSelection");
+  volcanoes = mapSvg.append("g")
     .attr("id", "volcanoes");
 
   let projection = d3.geoMercator()
@@ -33,25 +43,13 @@
     .scale(mapZoom)
     .translate([ mapWidth / 2, mapHeight / 2 ]);
   
-
-  // BRUSH STUFF
-
   let brush = d3.brush()
-    .extent([[-1000, -1000], [5000, 5000]])
-    .on("brush", (e) => {
-      if (regionSelectMode) {
-        regionMin = [(e.selection[0][0] - mapWidth / 2) / mapZoom - mapTranslateX, 
-                    (e.selection[0][1] - mapHeight / 2) / mapZoom - mapTranslateY];
-        regionMax = [(e.selection[1][0] - mapWidth / 2) / mapZoom - mapTranslateX, 
-                    (e.selection[1][1] - mapHeight / 2) / mapZoom - mapTranslateY];
-      }
-  });
+    .extent([[-1e5, -1e5], [1e5, 1e5]]);
 
-  brushSelection
-    .on("mousedown mousemove mouseover mouseleave", (e) => {
-        if (!regionSelectMode) e.stopPropagation();
-      }, true)
-    .call(brush);
+    
+  // #####################################
+  // ### MAP GENERATION AND NAVIGATION ###
+  // #####################################
 
   // populate svg with countries using the loaded geoData
   d3.json(dataPath + "custom.geo.json").then(function(geoData) {
@@ -67,7 +65,7 @@
   });
 
   // populate svg with volcanoes
-  d3.csv(dataPath + "GVP_Volcano_List.csv").then(function(volcanoData) {
+  d3.csv(dataPath + "GVP_Volcano_List_old.csv").then(function(volcanoData) {
       volcanoes.selectAll("circle")
         .data(volcanoData)
         .enter()
@@ -75,8 +73,12 @@
           .attr("cx", d => projection([d.Longitude, d.Latitude])[0])
           .attr("cy", d => projection([d.Longitude, d.Latitude])[1])
           .attr("r", volcanoIconSize);
-        
+      
+      // set up tooltip (can only be done after volcanoes are loaded)
       setupTooltip();
+
+      // call onMapReady events
+      onMapReadyEvents.forEach(e => e());
   });
 
   // map update function for when projection changes
@@ -85,26 +87,50 @@
       .scale(mapZoom)
       .translate([mapWidth / 2 + mapTranslateX * mapZoom, mapHeight / 2 + mapTranslateY * mapZoom]);
     
+    // redraw countries
     countries.selectAll("path")
       .attr("d", d3.geoPath()
         .projection(projection)
       );
     
+    // reposition volcanoes
     volcanoes.selectAll("circle")
     .attr("cx", d => projection([d.Longitude, d.Latitude])[0])
     .attr("cy", d => projection([d.Longitude, d.Latitude])[1])
 
+    // reposition region selection
     brush
-      .move(brushSelection, [[mapWidth / 2 + (regionMin[0] + mapTranslateX) * mapZoom, 
+      .move(regionSelection, [[mapWidth / 2 + (regionMin[0] + mapTranslateX) * mapZoom, 
                               mapHeight / 2 + (regionMin[1] + mapTranslateY) * mapZoom], 
                              [mapWidth / 2 + (regionMax[0] + mapTranslateX) * mapZoom, 
                               mapHeight / 2 + (regionMax[1] + mapTranslateY) * mapZoom]])
   }
 
-  // map update function for when eruption time range changes (defined below)
-  let updateTimeRange = () => undefined;
+  // set up zoom input
+  mapSvg.on("wheel", (e) => {
+    mapZoom = mapZoom ** (1 - e.deltaY * mapZoomfactor);
+    updateMapView();
+    return false;
+  });
 
-  d3.csv(dataPath + "GVP_Volcano_List.csv").then(function(volcanoData) {
+  // set up mouse drag input
+  mapSvg.on("mousemove", (e) => {
+    if (e.buttons == 1) { // check if left-button is held
+      mapTranslateX += e.movementX / mapZoom;
+      mapTranslateY += e.movementY / mapZoom;
+      updateMapView();
+    }
+  }, true);
+
+    
+  // ######################################
+  // ### MAP UPDATE BASED ON TIME RANGE ###
+  // ######################################
+
+  // map update function for when eruption time range changes (defined below)
+  let updateTimeRange = (min, max) => undefined;
+
+  d3.csv(dataPath + "GVP_Volcano_List_old.csv").then(function(volcanoData) {
     d3.csv(dataPath + "GVP_Eruption_Results.csv").then(function(eruptionData) {
       
       // set up eruption and volcano-eruption indices for quick lookup
@@ -129,7 +155,8 @@
           volcanoEruptionIndex[volcanoId].push(eruptionId);
       });
 
-      updateTimeRange = () => {
+      // now that data is prepared, define the time range update function
+      updateTimeRange = (min, max) => {
         volcanoes.selectAll("circle")
           .style("fill", d => {
             let eruptions = volcanoEruptionIndex[d.Volcano_Number];
@@ -137,34 +164,24 @@
             let inRange = eruptions.some(eruptionId => {
               let eruption = eruptionIndex[eruptionId];
               
-              return parseFloat(eruption["Start Year"]) >= eruptionTimeRange[0] 
-                && parseFloat(eruption["Start Year"]) <= eruptionTimeRange[1]
+              return parseFloat(eruption["Start Year"]) >= min 
+                && parseFloat(eruption["Start Year"]) <= max
             });
 
             return inRange ? volcanoEruptedColor : volcanoNotEruptedColor;
           });
       };
 
-      updateTimeRange();
+      // call the updateTimeRange function once to correctly color the volcanoes
+      updateTimeRange(-Infinity, Infinity);
     })
   });
-
-  // set up zoom input
-  mapSvg.on("wheel", (e) => {
-    mapZoom = mapZoom ** (1 - e.deltaY * mapZoomfactor);
-    updateMapView();
-    return false;
-  });
-
-  // set up mouse drag input
-  mapSvg.on("mousemove", (e) => {
-    if (e.buttons == 1) { // check if left-button is held
-      mapTranslateX += e.movementX / mapZoom;
-      mapTranslateY += e.movementY / mapZoom;
-      updateMapView();
-    }
-  }, true);
   
+    
+  // ###############
+  // ### TOOLTIP ###
+  // ###############
+
   // add tooltip functionality
   function setupTooltip() {
     
@@ -183,29 +200,88 @@
         .style("left", (e.pageX+10) + "px")
         .style("top", e.pageY + "px");
       });
-  }
+    }
 
-  let clearRegionButton = d3.select('#clearRegionButton')
-    .on("click", () => {
-      brush.clear(brushSelection);
-      regionMin = [0, 0];
-      regionMax = [0, 0];
+    
+// ########################
+// ### REGION SELECTION ###
+// ########################
+
+// utility function to check if a volcano circle is inside the selection region
+isVolcanoInsideRegion = (c, overrideSelection = null) => {
+  let selection = overrideSelection != null 
+    ? overrideSelection
+    : d3.brushSelection(regionSelection.node());
+
+  return selection == null                                                // if selection is null
+    || c.attr("cx") >= selection[0][0] && c.attr("cy") >= selection[0][1] // or circle inside
+    && c.attr("cx") <= selection[1][0] && c.attr("cy") <= selection[1][1];
+}
+
+// define what happens when the selection region is adjusted by the user
+brush.on("brush end", (e) => { if (regionSelectMode) update(e.selection); });
+
+function update(selection) {
+  
+  // Capture zoom-invariant region variables regionMin and regionMax
+  regionMin = selection != null
+    ? [(selection[0][0] - mapWidth / 2) / mapZoom - mapTranslateX, 
+        (selection[0][1] - mapHeight / 2) / mapZoom - mapTranslateY]
+    : [0, 0];
+  regionMax = selection != null
+    ? [(selection[1][0] - mapWidth / 2) / mapZoom - mapTranslateX, 
+      (selection[1][1] - mapHeight / 2) / mapZoom - mapTranslateY]
+    : [0, 0];
+
+  // Adjust circle opacity based on selection
+  volcanoes.selectAll("circle")
+    .each((_, i, circles) => {
+      let c = d3.select(circles[i]);
+      c.style("fill-opacity", isVolcanoInsideRegion(c, selection) ? 0.5 : 0);
     });
   
-  d3.select('#selectRegionButton')
+  // Call other events
+  onRegionAdjustEvents.forEach(e => e());
+}
+
+// Call onRegionAdjust once at start up with an empty selection
+update(null);
+
+// attach brush to regionSelection svg and prevent interaction when regionSelectMode == false
+regionSelection
+  .on("mousedown mousemove mouseover mouseleave", (e) => {
+      if (!regionSelectMode) e.stopPropagation();
+    }, true)
+  .call(brush);
+
+  // set up functionality of "clear region" button
+  let clearRegionButton = d3.select('#clearRegionButton')
     .on("click", () => {
-      regionSelectMode = !regionSelectMode;
-      brushSelection
-        .select(".overlay")
-        .style("cursor", regionSelectMode ? "crosshair" : "move");
-      
-      d3.select('#selectRegionButton')
-        .html(regionSelectMode ? "done" : "region select");
-
-      clearRegionButton
-        .attr("disabled", regionSelectMode ? true : null);
-
-      volcanoes
-        .attr("pointer-events", regionSelectMode ? "none" : "auto");
+      brush.clear(regionSelection);
+      update(null);
     });
+
+  // set up functionality of "select region" button
+  function selectRegionButtonOnClick() {
+    regionSelectMode = !regionSelectMode;
+    regionSelection
+      .select(".overlay")
+      .style("cursor", regionSelectMode ? "crosshair" : "move");
+    
+    d3.select('#selectRegionButton')
+      .html(regionSelectMode ? "done" : "region select");
+
+    clearRegionButton
+      .attr("disabled", regionSelectMode ? true : null);
+
+    volcanoes
+      .attr("pointer-events", regionSelectMode ? "none" : "auto");
+  }
+  
+  d3.select('#selectRegionButton')
+    .on("click", selectRegionButtonOnClick);
+  
+  // call this function once at the start (assuming regionSelectMode == true) to set cursors 
+  // correctly.
+  selectRegionButtonOnClick();
 }
