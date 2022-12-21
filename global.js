013c926e8707b5295a60bba68d1f2393d4015e28
Joost-Jansen <joost.jjj@gmail.com>
2022-12-21 14:15:35 +0100

    made map and scatter working together

--- global.js
+++ global.js
@@ -1,5 +1,7 @@
 // interface for map.js
 let volcanoes = undefined;
+let scatter =  undefined
 let isVolcanoInsideRegion = (c) => undefined;
 let onRegionAdjustEvents = [];
 let onMapReadyEvents = [];
+let volcanoIconSize = undefined;
